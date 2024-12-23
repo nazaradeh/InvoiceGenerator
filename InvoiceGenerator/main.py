3fed8b1e32e60b774c326b5797cff30ed8077159
Aryo Nazaradeh <aryo.nazaradeh@gmail.com>
2024-12-22 21:03:01 -0800

    Centre UI elements

--- InvoiceGenerator/main.py
+++ InvoiceGenerator/main.py
@@ -17,29 +17,40 @@ def add_row():
     table_of_items.append([item, rate, quantity, amount])
 
 root = tk.Tk()
-root.geometry("600x500")
+root.geometry("550x310")
 root.title("Invoice Generator")
+root.resizable(False, False)
 
-tk.Label(root, text="Invoice Number").grid(row=0, column=0, pady=5)
-tk.Entry(root, width=15).grid(row=1, column=0, pady=5)
+container = tk.Frame(root)
+container.pack(expand=True, fill="both", padx=20)
 
-tk.Label(root, text="Client Information").grid(row=0, column=1, pady=5)
-tk.Text(root, width=15, height=5).grid(row=1, column=1, pady=5)
+row_1 = tk.Frame(container)
+row_1.grid(row=0, column=0, pady=10)
 
-tk.Label(root, text="Item").grid(row=2, column=0, pady=5)
-tk.Entry(root, width=30).grid(row=3, column=0, pady=5)
+tk.Label(row_1, text="Invoice Number").grid(row=0, column=0, padx=5, pady=5)
+tk.Entry(row_1, width=15).grid(row=1, column=0, padx=5, pady=5, sticky="n")
 
-tk.Label(root, text="Rate").grid(row=2, column=1, pady=5)
-tk.Entry(root, width=15).grid(row=3, column=1, pady=5)
+tk.Label(row_1, text="Client Information").grid(row=0, column=1, padx=5, pady=5)
+tk.Text(row_1, width=15, height=5).grid(row=1, column=1, padx=5, pady=5)
 
-tk.Label(root, text="Quantity").grid(row=2, column=2, pady=5)
-tk.Entry(root, width=10).grid(row=3, column=2, pady=5)
+row_2 = tk.Frame(container)
+row_2.grid(row=1, column=0, pady=10)
 
-tk.Label(root, text="Amount").grid(row=2, column=3, pady=5)
-tk.Entry(root, width=20).grid(row=3, column=3, pady=5)
 
-confirmation_button = tk.Button(root, text="Generate", command=lambda: None)
-confirmation_button.grid(row=4, column=0, columnspan=2, pady=20)
+tk.Label(row_2, text="Item").grid(row=0, column=0, padx=5, pady=5)
+tk.Entry(row_2, width=30).grid(row=1, column=0, padx=5, pady=5)
+
+tk.Label(row_2, text="Rate").grid(row=0, column=1, padx=5, pady=5)
+tk.Entry(row_2, width=15).grid(row=1, column=1, padx=5, pady=5)
+
+tk.Label(row_2, text="Quantity").grid(row=0, column=2, padx=5, pady=5)
+tk.Entry(row_2, width=10).grid(row=1, column=2, padx=5, pady=5)
+
+tk.Label(row_2, text="Amount").grid(row=0, column=3, padx=5, pady=5)
+tk.Entry(row_2, width=20).grid(row=1, column=3, padx=5, pady=5)
+
+confirmation_button = tk.Button(container, text="Generate", command=lambda: None)
+confirmation_button.grid(row=2, column=0, columnspan=2, pady=20)
 
 
 root.mainloop()
